0fa81df0e461b87c491189202d1c086ac2c4bc17
Jakub Serwatka <kubaserw@gmail.com>
2020-08-28 11:12:42 +0200

    First draft of models created

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+
+# User table - visible columns for admin view
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "password")
+
+admin.site.register(User, UserAdmin)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,55 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Auction(models.Model):
+    # Categories - choices
+    MOTORS = "MOT"
+    FASHINON = "FAS"
+    ELECTRONICS = "ELE"
+    COLLECTIBLES_ARTS = "ART"
+    HOME_GARDES = "HGA"
+    SPORTING_GOODS = "SPO"
+    TOYS = "TOY"
+    BUSSINES_INDUSTRIAL = "BUS"
+    MUSIC = "MUS"
+
+    CATEGORY = [
+        (MOTORS, "Motors"),
+        (FASHINON, "Fashion"),
+        (ELECTRONICS, "Electronics"),
+        (COLLECTIBLES_ARTS, "Collectibles & Art"),
+        (HOME_GARDES, "Home & Garden"),
+        (SPORTING_GOODS, "Sporting Goods"),
+        (TOYS, "Toys"),
+        (BUSSINES_INDUSTRIAL, "Business & Industrial"),
+        (MUSIC, "Music"),
+    ]
+
+    # Model fields
+    # auto: auction_id
+    # fk: user_Id #TODO
+    category = models.CharField(max_length=3, choices=CATEGORY, default=MOTORS)
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+class Bid(models.Model):
+    # Model fields
+    # auto: bid_id
+    # fk: auction_id #TODO
+    # fk: user_id #TODO
+    bid_date = models.DateTimeField(auto_now_add=True)
+    bid_price = models.DecimalField(max_digits=11, decimal_places=2)
+
+class Comment(models.Model):
+    # Model fields
+    # auto: comment_id
+    # fk: auction_id #TODO
+    # fk: user_id #TODO
+    comment = models.TextField()
+
+class Watchlis(models.Model):
+    # Model field
+    # auto: watchlist_id
+    # fk: auction_id #TODO
+    # fk: user_id #TODO
+    pass
