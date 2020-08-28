6a498f71c6d22b78213e13b2e116057beca6374a
Jakub Serwatka <kubaserw@gmail.com>
2020-08-28 15:33:08 +0200

    current_price added to Auction model

--- auctions/models.py
+++ auctions/models.py
@@ -34,6 +34,7 @@ class Auction(models.Model):
     seller_id = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=64, blank=False)
     description = models.TextField(blank=True)
+    current_price = models.DecimalField(max_digits=11, decimal_places=2, default=0)
     category = models.CharField(max_length=3, choices=CATEGORY, default=MOTORS)
     image_url = models.URLField(blank=True)
     publication_date = models.DateTimeField(auto_now_add=True)
